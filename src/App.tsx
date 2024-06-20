3422e21eaeea4901656889f8c7933a798e835be5
twój login do GitHuba <twj email>
2024-06-20 10:16:04 +0200

    ZS-1 / app wrapper in ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
+import * as React from 'react'; 
+import { ThemeProvider } from '@mui/material/styles'; 
+import CssBaseline from '@mui/material/CssBaseline'; 
+import theme from './styles/theme';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
+function App({ children }: { children: React.ReactNode }) {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <ThemeProvider theme={theme}> 
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */} 
+    <CssBaseline /> 
+    {children} 
+    <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,7 +36,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+  </ThemeProvider> 
   )
 }
 
