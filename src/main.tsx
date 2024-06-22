d893ca2671875360d11eeb8327ee61fe460cf807
twój login do GitHuba <twj email>
2024-06-22 22:12:49 +0200

    ZS-2 / sure that document.getElementById is never null

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
  
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <Auth0Provider
   domain='dev-30ax000lzfhvtghw.us.auth0.com'
