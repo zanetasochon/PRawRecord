f538a3b7416a1d8cc04d50b6025d52a9aa794b75
Zaneta Sochon <twj email>
2025-02-02 11:12:59 +0100

    Test
    Test of git commands
    
    [Ticket: 1234]

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { ThemeProvider } from '@mui/material/styles'; 
-import CssBaseline from '@mui/material/CssBaseline'; 
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import theme from './styles/theme';
-import './App.css'
-import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Auth0ProviderWithNavigate from './auth/auth0Provider';
 
 const App = () => {
-
   return (
     <Auth0ProviderWithNavigate>
-    <ThemeProvider theme={theme}> 
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */} 
-    <CssBaseline /> 
-    <Router>
-        <Navbar />
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. TEST*/}
+        <CssBaseline />
+        <Router>
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
-    </Router>
-    </ThemeProvider> 
+        </Router>
+      </ThemeProvider>
     </Auth0ProviderWithNavigate>
   );
 };
